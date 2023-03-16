389a7b8737435c28291a7a5c2d8b4653a84d7ffc
Pavel Tížek <pavel@tizek.cz>
2023-03-16 09:40:13 +0100

    Fixed constructor params order

--- src/Command/FeedCommand.php
+++ src/Command/FeedCommand.php
@@ -18,7 +18,7 @@ class FeedCommand extends Command {
 	/** @var array */
 	private $config;
 
-	public function __construct(array $config = array(), Nette\DI\Container $container)
+	public function __construct(Nette\DI\Container $container, array $config = array())
 	{
 		parent::__construct();
 
